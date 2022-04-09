f9a8d68d3136bc312cb10a2497e9a249ed8972e9
TomSuzuki <tomsuzuki2633@gmail.com>
2022-04-09 16:12:32 +0900

    [add] メニュー。

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -1,3 +1,6 @@
 import { works_page } from "./works.js";
+import { menu_scroll } from "./menu.js";
 
 window.works_page = works_page;
+window.menu_scroll = menu_scroll;
+
---
+++ docs/assets/js/menu.js
@@ -0,0 +1,6 @@
+import { scroll_to_id } from "./common.js";
+
+// menu_scroll ...for menu.
+export function menu_scroll(anchor) {
+    scroll_to_id(anchor, -16);
+}
